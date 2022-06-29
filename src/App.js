5ea284497dcff705af5cbe28b8554b9cab17d58a
Aausaf Alam <aausafalam585@gmail.com>
2022-06-29 06:49:53 -0700

    assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { store } from './redux/state';
+import {useSelector , useDispatch} from 'react-redux'
+import { logins, profile, reg } from './redux/action';
+import { useState } from 'react';
+import Registercomp from './component/registercomp';
+import Login from './component/Login';
+import Profile from './component/profile';
+import {Route,Routes} from 'react-router-dom'
+import Navbar from './component/Navbar';
+import Registerresponse from './component/Registerresponse';
+import Loginres from './component/Loginres';
 function App() {
+  const {register,profiles} = useSelector((store)=>store)
+  const dispatch = useDispatch()
+  const [a,seta] = useState(true)
+ 
+  if(a)
+  {
+    dispatch(profile("wefouhowerh"))
+    dispatch(reg("dwlfheou y7w"))
+    dispatch(logins("efoaufy9ny9 eq"))
+    
+    seta(false)
+  }
+ // console.log(register)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+  <Routes>
+    
+    <Route path="/" element={<Registercomp></Registercomp>}/>
+    <Route path="/Login" element={<Login></Login>}/>
+    <Route path='/profile' element={<Profile></Profile>}/>
+    <Route path="/regresponse" element={<Registerresponse></Registerresponse>}/>
+    <Route path='/loginres' element={<Loginres></Loginres>}/>
+  </Routes>
     </div>
   );
 }
